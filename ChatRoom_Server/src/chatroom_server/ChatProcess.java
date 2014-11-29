409cf0293725f5bc73c5bf2d42bf294c631e4e2b
Tran Ba Y <tranbay.tranbay@gmail.com>
2014-11-30 00:37:32 +0700

    Tạo Contructor và viết hàm run

--- ChatRoom_Server/src/chatroom_server/ChatProcess.java
+++ ChatRoom_Server/src/chatroom_server/ChatProcess.java
@@ -5,7 +5,10 @@
  */
 package chatroom_server;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.net.Socket;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,15 +16,63 @@ import java.net.Socket;
  */
 public class ChatProcess  implements Runnable{
 
-    ChatProcess(Socket socket, ServerView aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    DataInputStream in;
+    Socket clientSocket;
+    ServerView mFrame;
+    String readStr;
+    Thread thread;
+    String UserName="Không Tên",RoomName = "Phòng Ý Tưởng";
+    int i=0,Index1,Index2;
+    ChatProcess(Socket socket, ServerView mainFrame) {
+       clientSocket = socket;
+        mFrame = mainFrame;
+        
+        try{
+            
+            in = new DataInputStream((new BufferedInputStream(clientSocket.getInputStream())));
+            
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, " Lỗi trong Contructor" + e.getMessage());
+            thread.stop();
+            thread =null;
+            socket = null;
+            
+        }
+        thread = new Thread(this);
+        thread.start();
+        
+        
     }
 
     @Override
     public void run() {
+        while(thread != null){
+            
+            try{
+                readStr = in.readUTF();
+                System.out.println(readStr);
+                if(readStr.startsWith("HELLO")){
+                    Index1 = readStr.indexOf("::") +2;
+                    Index2 = readStr.indexOf("  ");
+                    UserName = readStr.substring(Index1, Index2);
+                    mFrame.IncConnectionCount();
+                    
+                    
+                    
+                    
+                }
+                
+                
+            }
+            catch(Exception e){
+                
+            }
+        }
         
         
     }
+    
     
     
 }
